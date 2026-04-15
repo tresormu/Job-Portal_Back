6961ff3d64b919809602849f010ef574f8d0c95f
tresor <tresormugisha07@gmail.com>
2026-04-15 17:38:16 +0200

    cors removal

--- src/app.ts
+++ src/app.ts
@@ -16,11 +16,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (!config.corsOrigins || config.corsOrigins.includes(origin)) return callback(null, true);
-      callback(new Error(`CORS: origin ${origin} not allowed`));
-    },
+    origin:[],
     credentials: true,
   })
 );
